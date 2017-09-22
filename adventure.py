bb5d5c5115a07b6e377b7c40e6f05c5e63a1e10e
NEAL LARRY D <lnea1902@education.mpls.k12.mn.us>
2017-09-22 13:39:52 -0500

    added adventure.py

---
+++ adventure.py
@@ -0,0 +1,28 @@
+g# -*- coding: utf-8 -*-
+print "One day i was walking to my brother’s house."
+raw_input()
+print 'As i was walking, a black cat walked in front of me.'
+raw_input()
+print 'I was always told if a black cat walk in front of your path, you should back up and go the other way, cause its considered bad luck.'
+raw_input()
+print 'Since i know everything, i thought it was only if you was in a car.'
+raw_input()
+print 'So i kept walking to his house, as i was getting closer, it started raining out of nowhere.'
+raw_input() 
+print 'Some I started running to his house so i wouldnt get fully wet.'
+raw_input()
+print 'Then as i got even closer to his place, the fastest way to his house was blocked off, as it something happened down there where police wouldnt let nobody in or out of the area/block.'
+raw_input()
+print 'So I had to go through the woods(wasnt my best idea, but i didnt want to be in the rain for a long time.)'
+raw_input() 
+print 'So I tried to run as fast as i could through the woods to get their cause i got tired of being outside.'
+raw_input()
+print 'All of a sudden i ended up on the ground, i got up and started looking for what i feel over,and i looked down and it was air.'
+raw_input()
+print 'So i started walking again, cause at that point i didnt care bout getting wet.'
+raw_input()
+print 'I just wanted to make sure i was safe.10Min la ter, i affiverd at his house and everything went back to normal.'
+raw_input()
+print 'The sun was out, and everything was all good.'
+raw_input()
+print 'Then i thought everything was not normal.' 
